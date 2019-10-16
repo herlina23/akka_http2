7ff645732af3f799b5a73fd60276e455226072d5
herlina23 <frois.herlina@gmail.com>
2019-10-16 15:50:55 +0700

    JSON already parsed

--- src/main/scala/new3/stream1.scala
+++ src/main/scala/new3/stream1.scala
@@ -12,6 +12,56 @@ import scala.concurrent.Future
 import scala.util.{Failure, Success}
 import akka.stream.scaladsl.{FileIO, Sink, Source}
 import weatherForecast.Forecast
+import akka.util.ByteString
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
+import play.api.libs.json.{JsError, JsSuccess, Json}
+import akka.stream.alpakka.file.scaladsl.Directory
+import java.nio.file.{Files, Path}
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
+import play.api.libs.json.{JsError, JsSuccess, Json}
+import scala.util.Success
+import scala.util.Failure
+import akka.NotUsed
+import java.nio.file.{Files, Paths}
+import akka.stream._
+import akka.stream.scaladsl.{FileIO, Sink, Source}
+import akka.util.ByteString
+import scala.concurrent.Future
+import java.io._
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
+import play.api.libs.json.{JsError, JsSuccess, Json}
+import scala.util.Success
+import scala.util.Failure
+import akka.NotUsed
+import java.nio.file.{Files, Paths}
+import akka.stream._
+import akka.stream.scaladsl.{FileIO, Sink, Source}
+import akka.util.ByteString
+import scala.concurrent.Future
+import akka.Done
 
 //res.entity jadikan suatu jsvalue
 
@@ -27,21 +77,38 @@ object Stream1 {
     responseFuture
       .onComplete {
         case Success(res) =>
-          val ddf = res.entity.dataBytes
+          val jsonBody = res.entity.dataBytes
+            .runFold(ByteString(""))(_ ++ _)
             .map(_.utf8String)
             .map{
               str =>
                 val jsonValue = Json.parse(str)
                 val forecastOpt = jsonValue.asOpt[Forecast]
                 forecastOpt
+                //jsonValue
             }
-            .runForeach(println)
+           // .runForeach(println)
 
-          res.discardEntityBytes()
-          //res.entity
+          val hasil = jsonBody.map { coba =>
+            println(coba)
+          }
+
+          for {
+            _ <- hasil.recover {
+              case e: Throwable =>
+                system.log.error(e, "Error on running main process!")
+
+            }
+            _ <- system.terminate()
+          } yield {
+            sys.exit()
+          }
+
+//          res.discardEntityBytes()
+//          //res.entity
         case Failure(_)   => sys.error("something wrong")
       }
   }
 }
 
-case class ByteString(str: String)
+//case class ByteString(str: String)
